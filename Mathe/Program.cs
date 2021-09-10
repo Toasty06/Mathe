b0ac89ea3cfd018bc637a37094a09cc0189e8f51
Toasty06 <to.stoiber@gmail.com>
2021-09-10 23:07:06 +0200

    PFZ added NOT COMPLETE QE added NOT COMPLETE

--- Mathe/Program.cs
+++ Mathe/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Linq;
 
 namespace Primzahl
 {
@@ -12,6 +14,8 @@ namespace Primzahl
             {
                 Console.WriteLine("Primzahl: 1");
                 Console.WriteLine("Nullstellen: 2");
+                Console.WriteLine("Primfaktoren: 3");
+                Console.WriteLine("Quadratische Ergänzung: 4");
                 switch (Console.ReadLine())
                 {
                     case "2":
@@ -23,32 +27,34 @@ namespace Primzahl
                         String b2 = Console.ReadLine();
                         Console.Write("    +");
                         String c2 = Console.ReadLine();
-
-                        long a3 = (long)Convert.ToDouble(a2);
-                        long b3 = (long)Convert.ToDouble(b2);
-                        long c3 = (long)Convert.ToDouble(c2);
-                        long D = (b3 * b3) - 4 * a3 * c3;       //Diskriminante
-                        double WD = Math.Sqrt(D);               //Wurzel Diskriminante
-                        decimal ac = 1 / (a3 * 2M);             //1. Teil
-                        double acd = Convert.ToDouble(ac);
-                        double x1 = Math.Round((acd * (-b3 + WD)), 3);     //2. Teil
-                        double x2 = Math.Round((acd * (-b3 - WD)), 3);
-                        if (D < 0)
+                        try
                         {
-                            Console.WriteLine("Keine Lösung");
-                        }
-                        else if (D == 0)
-                        {
-                            Console.WriteLine("Nullstelle: " + "x=" + x1);
-                        }
-                        else if (D > 0)
-                        {
-                            Console.WriteLine("Nullstellen: " + "x1=" + x1);
-                            Console.WriteLine("             x2=" + x2);
-                        }
+                            long a3 = (long)Convert.ToDouble(a2);
+                            long b3 = (long)Convert.ToDouble(b2);
+                            long c3 = (long)Convert.ToDouble(c2);
+                            long D = (b3 * b3) - 4 * a3 * c3;       //Diskriminante
+                            double WD = Math.Sqrt(D);               //Wurzel Diskriminante
+                            decimal ac = 1 / (a3 * 2M);             //1. Teil
+                            double acd = Convert.ToDouble(ac);
+                            double x1 = Math.Round((acd * (-b3 + WD)), 3);     //2. Teil
+                            double x2 = Math.Round((acd * (-b3 - WD)), 3);
+                            if (D < 0)
+                            {
+                                Console.WriteLine("Keine Lösung");
+                            }
+                            else if (D == 0)
+                            {
+                                Console.WriteLine("Nullstelle: " + "x=" + x1);
+                            }
+                            else if (D > 0)
+                            {
+                                Console.WriteLine("Nullstellen: " + "x1=" + x1);
+                                Console.WriteLine("             x2=" + x2);
+                            }
 
-                        Console.WriteLine("----------------------------");
-
+                            Console.WriteLine("----------------------------");
+                        }
+                        catch { }
                         break;
 
                     case "1":
@@ -68,43 +74,143 @@ namespace Primzahl
 
                             Console.Write("Zahl: ");
                             string i = Console.ReadLine();
-                            int z = Int32.Parse(i);
-                            int q = QS(z);
-                            int a = z % 2; //durch 2
-                            if (a == 0) { pz = false; }
-                            int b = q % 3; //durch 3
-                            if (b == 0) { pz = false; }
-                            int c = z % 5; //durch 5
-                            if (c == 0) { pz = false; }
-                            int d = z % 7;
-                            if (d == 0) { pz = false; }
-                            if (z == 1)
+                            try
                             {
-                                pz = false;
-                            }
+                                int z = Int32.Parse(i);
 
-                            //console
-                            if (z == 2 || z == 3 || z == 5 || z == 7 || pz == true)
+                                int q = QS(z);
+                                int a = z % 2; //durch 2
+                                if (a == 0) { pz = false; }
+                                int b = q % 3; //durch 3
+                                if (b == 0) { pz = false; }
+                                int c = z % 5; //durch 5
+                                if (c == 0) { pz = false; }
+                                int d = z % 7;
+                                if (d == 0) { pz = false; }
+                                if (z == 1)
+                                {
+                                    pz = false;
+                                }
+
+                                //console
+                                if (z == 2 || z == 3 || z == 5 || z == 7 || pz == true)
+                                {
+                                    Console.WriteLine(o);
+                                    Console.WriteLine("Primzahl");
+                                    Console.WriteLine("----------------------------");
+
+                                }
+                                else
+                                {
+                                    Console.WriteLine(o);
+                                    Console.WriteLine("keine Primzahl");
+                                    Console.WriteLine("----------------------------");
+                                }
+
+                            }
+                            catch { }
+                            }
+                        break;
+                    case "3":
+                        Console.WriteLine("----------------------------");
+                        Console.Write("Zahl: ");
+                        try
+                        {
+                            int zul = Int32.Parse(Console.ReadLine());
+
+                            foreach (int i in PFZ(zul))
                             {
-                                Console.WriteLine(o);
-                                Console.WriteLine("Primzahl");
-                                Console.WriteLine("----------------------------");
-
+                                Console.WriteLine(i);
+                            };
+                            String zih = Console.ReadLine();
+                            try
+                            {
+                                long zahl = (long)Convert.ToDouble(zih);
+                                int zahlint = (int)Convert.ToDouble(zih);
                             }
+                            catch { }
+
+                            Console.WriteLine("----------------------------");
+                        }
+                        catch { }
+                        break;
+                    case "4":
+                        Console.WriteLine("----------------------------");
+                        Console.WriteLine("Term: ");
+                        Console.Write("y = (x^2)*");
+                        String a4 = Console.ReadLine();
+                        Console.Write("    +x*");
+                        String b4 = Console.ReadLine();
+                        Console.Write("    +");
+                        String c4 = Console.ReadLine();
+                        try
+                        {
+                            long a5 = (long)Convert.ToDouble(a4);
+                            long b5 = (long)Convert.ToDouble(b4);
+                            long c5 = (long)Convert.ToDouble(c4);
+                            ArrayList same = new ArrayList();
+
+                            if (a5 % b5 == 0)
+                            {
+
+                                foreach (int i in PFZ(a5))
+                                {
+                                    if (PFZ(b5).Contains(i))
+                                    {
+                                        same.Add(i);
+                                        Console.WriteLine(i);
+                                        Console.WriteLine(same);
+                                    }
+                                }
+                                foreach (int i in PFZ(b5))
+                                {
+                                    if (PFZ(a5).Contains(i))
+                                    {
+                                        same.Add(i);
+                                        Console.WriteLine(i);
+                                        Console.WriteLine(same);
+                                    }
+                                }
+                            }
+                           
                             else
                             {
-                                Console.WriteLine(o);
-                                Console.WriteLine("keine Primzahl");
-                                Console.WriteLine("----------------------------");
+                                Console.WriteLine("fucked");
                             }
-
+                            Console.WriteLine("----------------------------");
                         }
+                        catch { }
                         break;
+
                     default:
                         break;
                 }
 
             }
+        }
+        public static ArrayList PFZ(long zahl)
+        {
+            int[] array3 = { 2, 3, 5, 7, 11, 13, 17, 19, 23 };
+            ArrayList Faktoren = new ArrayList();
+
+            if (array3.Contains((int)Convert.ToDouble(zahl)))
+            {
+                Console.WriteLine("keine Zerlegung möglich");
+
+            }
+            else
+            {
+                //Zerlegen
+                foreach (int i in array3)
+                {
+                    if (zahl % i == 0)
+                    {
+                        Faktoren.Add(i);
+                    }
+
+                }
+            }
+            return Faktoren;
         }
     }
 }
